f68fa9d20a619e09c0dd0103c9386bf0bb47acfb
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-13 16:01:19 -0700

    Add sql for reference

---
+++ data/rd_create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE public.employees (
+    employee_id INTEGER,
+    first_name  VARCHAR(64),
+    last_name   VARCHAR(64),
+    department  VARCHAR(64),
+    hire_date   DATE,
+    salary      DECIMAL(10,2)
+);
