e0a3886e9a2c06a4f1d06526611972df03b8dab4
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-31 21:38:06 -0700

    Create test script to write a test file into the S3 bucket

---
+++ app/src/app/main.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from app.utils.s3_service import upload_file
+from app.config import config
+
+CURRENT_DIR = Path(__file__).parent
+FILE_PATH = CURRENT_DIR / "test-data.txt"
+FILE_NAME = str(FILE_PATH)
+
+BUCKET_NAME = config.get("S3_BUCKET_NAME")
+if __name__ == "__main__":
+
+    success = upload_file(FILE_NAME, BUCKET_NAME)
+    if success:
+        print(f"File {FILE_NAME} uploaded successfully to bucket {BUCKET_NAME}.")
+    else:
+        print(f"Failed to upload file {FILE_NAME} to bucket {BUCKET_NAME}.")
